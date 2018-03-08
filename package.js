c862721a845b3929214524308b7ba4f851fe1e69
Massimo Menichinelli <info@openp2pdesign.org>
2018-03-09 00:13:09 +0100

    Bump to 0.4.8, 0.4.3 in cesarve:simple-chat

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-    summary: "Simple chat window. The star point to make your own chat",
-    version: "0.4.3",
-    name: "cesarve:simple-chat",
-    git: "https://github.com/cesarve77/simple-chat"
+    summary: "Simple chat window. The star point to make your own chat. Forked from cesarve:simple-chat",
+    version: "0.4.8",
+    name: "openp2pdesign:simple-chat",
+    git: "https://github.com/openp2pdesign/simple-chat/tree/mypackage"
 });
 Package.onUse(function (api) {
     api.versionsFrom('1.4');
